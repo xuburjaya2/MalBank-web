16a0b5e7f79b1d2d7e90a369620e271d38bce643
xuburjaya2 <121552799+xuburjaya2@users.noreply.github.com>
2023-03-14 16:39:21 +0700

    Initial commit

---
+++ application/views/features.php
@@ -0,0 +1,1 @@
+Fitur yang akan kamu dapatkan
---
+++ application/views/home.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="with=device-width, intial-scale=1.0">
+        <title>MalBank | Dimana ada kendala MalBank solusinya</title>
+        <link rel="stylesheet" href="<?= base_url ("assets/style.css")?>">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=
+Poppins:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href='https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' >   
+    </head>
+    <body>
+        <section class="header">
+        <nav>
+            <a href="index.html"><img src="<?= base_url("assets/images/logo3.png")?>" </a>
+            <div class="nav-links" id="navLinks">
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="">HOME</a></li> 
+                    <li><a href="">FEATURES</a></li>
+                    <li><a href="">ABOUT US</a></li>
+                    <li><a href="">NEWS</a></li>
+                    <li><a href="">CONTACT US</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>
+        </nav>
+    <div class="text-box">
+        <h1> Apapun Transaksinya</h1>
+    <div class="text-box2"><h1>MalBank</h1></div>
+        <h1>Kuncinya</h1>
+        <p>Dalam memulai perjalanan finansial, MalBank siap nih jadi teman menabung kamu, <br>
+            sekaligus teman beribadah dalam melakukan transaksi secara syariah <br>
+            Karena, MalBank mau kamu lebih aman, nyaman dan semangat buat wujudkan cita.</p>
+        <a href="" class="hero-btn">Visit Us To know More</a>
+    </div>
+        </section>
+
+<!-- Course -->
+<section class="course" >
+    <h1>Courses We Offer</h1>
+    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    
+    <div class="row">
+        <div class="course-col">
+            <h3>Intermediate</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur 
+                adipiscing elit Lorem Ipsum 
+                dolor sit amet, consectetur adipiscing elit
+            </p>
+        </div>
+        <div class="course-col">
+            <h3>Degree</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur 
+                adipiscing elit Lorem Ipsum 
+                dolor sit amet, consectetur adipiscing elit
+            </p>
+        </div>
+        <div class="course-col">
+            <h3>Post guardian</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur 
+                adipiscing elit Lorem Ipsum 
+                dolor sit amet, consectetur adipiscing elit
+            </p>
+        </div>
+    </div>
+</section>
+
+<!-- Campus -->
+
+<section class="campus">
+    <h1>Our Features</h1>
+    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+    <div class="row">
+        <div class="campus-col">
+            <img src="<?= base_url("assets/images/financial-profit.png")?>"
+            <div class="layer">
+                <h3>Financial</h3>
+            </div>
+
+        </div>
+        <div class="campus-col">
+            <img src="<?= base_url("assets/images/debit-card.png")?>"
+            <div class="layer">
+                <h3>Payment</h3>
+            </div>
+
+        </div>
+        <div class="campus-col">
+            <img src="<?= base_url('assets/images/fasilitas-merchant.png')?>"
+            <div class="layer">
+                <h3>Merchant</h3>
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<!-- fasilitas -->
+
+<section class="facilities">
+    <h1>Our Facilities</h1>
+    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+    <div class="row">
+        <div class="facilities-col">
+            <img src="<?= base_url("assets/images/hand.png")?>"
+            <h3>World Class Lib</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div class="facilities-col">
+            <img src="<?= base_url("assets/images/fasilitas-merchant.png")?>"
+            <h3>playground</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+        <div class="facilities-col">
+            <img src="<?= base_url("assets/images/fasilitas-uang.png")?>"
+            <h3>cafetaria</h3>
+            <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+        </div>
+    </div>
+</section>
+
+<!-- Testimoni -->
+<section class="testimoni">
+    <h1>What our Customers Says</h1>
+    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+    <div class="row">
+        <div class="testimoni-col">
+            <img src="<?= base_url("assets/images/cust udin.jpg")?>"
+                <div>
+                    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                    <h3>Udin syarif</h3>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-o"></i>
+                </div>
+        </div>
+        <div class="testimoni-col">
+            <img src="<?= base_url("assets/images/cust ady.jpg")?>"
+                <div>
+                    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                        Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+                    </p>
+                    <h3>Ady Gunawan</h3>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star"></i>
+                    <i class="fa fa-star-half-o"></i>
+                </div>
+        </div>
+    </div>
+</section>
+
+<!-- Call to Action -->
+
+<section class="CTA"> 
+    <h1>enrol for oir various online courses <br> anywhere from the World</h1>
+    <a href="" class="hero-btn">CONTACT US</a>
+</section>
+
+<!-- footer -->
+<section class="footer">
+    <h4>About Us</h4>
+    <p>Lorem Ipsum dolor sit amet, consectetur adipiscing elit
+        Lorem Ipsum dolor sit amet, consectetur adipiscing elit <br>
+        Lorem Ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <div class="icons">
+            <i class="fa fa-facebook"></i>
+            <i class="fa fa-twitter"></i>
+            <i class="fa fa-instagram"></i>
+        </div>
+        <p>made with <i class="fa fa-heart-o"></i> by Muhammad Akmal Fadhlullah</p>
+</section>
+
+        <!-- ------JavaScript for Toggle Menu------- -->
+        <script>
+            var navLinks= document.getElementById("navLinks")
+            function showMenu(){
+               navLinks.style.right = "0"; 
+            }
+            function hideMenu(){
+                navLinks.style.right= "-200px";
+            }
+
+        </script>
+</body>
+</html>
